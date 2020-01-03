590cd484b786c93014e1946bf86d3ce859bd8990
websitefordad <59204153+websitefordad@users.noreply.github.com>
2020-01-02 18:59:36 -0800

    Update functions to allow England mode

--- mapfns.js
+++ mapfns.js
@@ -16,8 +16,12 @@ function debounce(func, wait, immediate) {
 var map;
 var updateSize = debounce(function() {
   let metersPerPixel = 156543.03392 * Math.cos(map.center.lat() * Math.PI / 180) / Math.pow(2, map.zoom);
-  let kmPerPixel = metersPerPixel / 1000.0;
-  let kmWidthOfUk = 1078.0;
+  let kmWidthOfUk = getUrlParameter("country") == "england" ? 614.0 : 1078.0;
+  if (getUrlParameter("country") == "england") {
+    document.getElementById("country-image").src = "./englandoutline.png";
+  } else {
+    document.getElementById("country-image").src = "./ukoutline.png";
+  }
   let widthOfPic = (kmWidthOfUk * 1000) / metersPerPixel;
   widthOfPic = Math.floor(widthOfPic);
   let scale = widthOfPic / 200;
@@ -35,10 +39,15 @@ function initMap() {
   /*let url = new URL(window.location.href);
   let latFromUrlParam = url.searchParams.get("lat");
   let longFromUrlParam = url.searchParams.get("long");*/
+  let country = getUrlParameter("country");
+  let defaultlat = 54.786826051560574;
+  let defaultlong = -4.255790353064472;
+  let englandDefaultLat = 53.04919039252906;
+  let englandDefaultLong = -2.3661419155644126;
   let latFromUrlParam = getUrlParameter("lat");
   let longFromUrlParam = getUrlParameter("long");
-  latFromUrlParam = (latFromUrlParam) ? Number(latFromUrlParam) : 54.786826051560574;
-  longFromUrlParam = (longFromUrlParam) ? Number(longFromUrlParam) : -4.255790353064472;
+  latFromUrlParam = (latFromUrlParam) ? Number(latFromUrlParam) : (country == "england") ? englandDefaultLat: defaultlat;
+  longFromUrlParam = (longFromUrlParam) ? Number(longFromUrlParam) : (country == "england") ? englandDefaultLong : defaultlong;
   map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: latFromUrlParam, lng: longFromUrlParam},
       zoom: 4, scaleControl: true,
